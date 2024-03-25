779e943710ecb817faa4dd95dda0d86b1017db78
Huỳnh Khánh Duy <duyxanh2002@gmail.com>
2024-03-25 20:52:30 +0700

    flat list

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -10,9 +10,46 @@ import {
     StatusBar,
     Touchable,
     StyleSheet,
+    FlatList,
 } from 'react-native'
 import { images, icons } from '../constants/manager'
 import { green } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
+
+const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        name: 'Nike Air Max 90',
+        price: 239.80,
+        image: icons.adidas,
+        brand: icons.adidas,
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        name: 'Nike Air Max 80',
+        price: 239.80,
+        image: icons.adidas,
+        brand: icons.adidas,
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        name: 'Jordan Air',
+        price: 239.80,
+        image: icons.adidas,
+        brand: icons.adidas,
+    },
+];
+
+const Item = ({ price, name, image, brand }) => (
+    <View style={{ padding: 10, margin: 20, width: '30%', height: 100 }}>
+        <View style={{backgroundColor: '#CCCCCC', borderRadius: 20}}>
+            <Image source={image} resizeMode="stretch" />
+            <Image source={brand} resizeMode="stretch" style={{position: "relative", right:5, bottom: 5}} />
+        </View>
+
+        <Text style={{}}>{price}</Text>
+        <Text style={{}}>{name}</Text>
+    </View>
+);
 
 function HomeScreen({ navigation }) {
     // const { name } = route.params
@@ -67,9 +104,12 @@ function HomeScreen({ navigation }) {
 
                 {/* Polular */}
                 <View style={{ height: '80%' }}>
-
-
-
+                    <FlatList
+                        numColumns={2}
+                        data={DATA}
+                        renderItem={({ item }) => <Item name={item.name} price={item.price} image={item.image} brand={item.brand} />}
+                        keyExtractor={item => item.id}
+                    />
                 </View>
 
             </SafeAreaView>
